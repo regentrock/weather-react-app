eeb9d0b0557b29fd5cbf9156ef1f5bf6c958f493
regentrock <angeloguittar2017@gmail.com>
2026-03-19 20:19:46 -0300

    Adição de icon logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,10 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>Weather<span>App</span></h1>
+      <div className={styles.logoContainer}>
+        <img src="/transparent-logo.png" alt="Logotipo do WeatherApp" />
+        <h1 className={styles.logo}>Weather<span>App</span></h1>
+      </div>
 
       <button onClick={toggleTheme} className={styles.toggleButton}>
         <div className={`${styles.toggleTrack} ${isDark ? styles.darkToggleTrack : ''}`}>
